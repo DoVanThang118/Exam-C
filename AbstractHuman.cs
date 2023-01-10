ba182d404f8ac1da3b2096ca212f023578bec074
DoVanThang118 <102956076+DoVanThang118@users.noreply.github.com>
2023-01-10 19:55:21 +0700

    Add files via upload

---
+++ AbstractHuman.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ConsoleApp_Exam.Exercise_2
+{
+    internal abstract class AbstractHuman
+    {
+        public int weight;
+        public string name;
+        public abstract string Show();
+        public abstract void SetMe(int weight, string name );
+
+    }
+}
---
+++ TigerBase.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleApp_Exam.Exercise_2
+{
+    internal class TigerBase
+    {
+        public override void SetMe(string weight, string name)
+        {
+            name = "Tiger";
+            weight = "100";
+        }
+
+        public override string Show()
+        {
+            return ("Name: " + name + "," + "Weight: " + weight);
+        }
+    }
+}
