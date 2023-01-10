2fdb955de455a2a89f777b3b1faf9e7d07f45207
DoVanThang118 <102956076+DoVanThang118@users.noreply.github.com>
2023-01-10 22:12:14 +0700

    add

---
+++ ConsoleApp-Exam/Exercise 2/Lion.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_Exam.Exercise_2
+{
+    internal class Lion : AbstractHuman
+    {
+        public override void SetMe(int weight, string name)
+        {
+          
+        }
+
+        public override string Show()
+        {
+            return ("Name: " + name + "," + "Weight: " + weight);
+        }
+    }
+}
---
+++ ConsoleApp-Exam/Program.cs
@@ -0,0 +1,27 @@
+﻿using ConsoleApp_Exam.Exercise_2;
+using ConsoleApp_Exam.GeometryExample;
+using System.Text;
+
+public class Program
+{
+    static void Main1(string[] args)
+    {
+        Cylinder c = new Cylinder();
+        c.Radius = 38.64;
+        Console.WriteLine("Radius: "+ c.Radius);
+        c.Height = 22.48;
+        Console.WriteLine("Height: "+c.Height);
+        
+        Console.WriteLine(c.Result);
+    }
+
+    static void Main(string[] args)
+    {
+        Lion l = new Lion();
+        l.SetMe(200, "lion");
+        Console.WriteLine(l.Show);
+        Tiger t = new Tiger();
+        t.SetMe(100, "tiger");
+        Console.WriteLine(t.Show);
+    }
+}
